87a95e4e95fa118b5afd8fb7c29cf2a54851066a
Ashley Mattam <amattam427@gmail.com>
2021-11-30 01:31:04 -0500

    commit tree 57b1588b3077c208de2334afbe7f5370f24e6479

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === string.toLowerCase()
+    )
+  }
+  
+  function fuzzyMatch(array, testString) {
+    return array.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(array, stringName) {
+    return array.filter( record => record.name === stringName)
+  }
